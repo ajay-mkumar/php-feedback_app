678443388922770308f9557229506fc09a9f78c0
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2021-08-25 23:26:34 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=mysqli_connect('localhost','ajay','0911ajay@','feedback');
+$conn=mysqli_connect('remotemysql.com','WjVc8R2Mt7','Lc588Sf4tw','WjVc8R2Mt7');
 if(!$conn){
 	echo "error:" . mysqli_connect_error();
 }
