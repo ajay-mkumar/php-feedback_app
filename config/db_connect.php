cfa69fb1ad8769f6ff4844c1d313bb4439beb294
ajay muthukumaran <65906852+ajay-mkumar@users.noreply.github.com>
2021-09-20 17:52:27 +0530

    Change conn

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$conn=mysqli_connect('remotemysql.com','cCkQ3QCarg','X9WYlPz4J6','cCkQ3QCarg');
+//$conn=mysqli_connect('remotemysql.com','cCkQ3QCarg','X9WYlPz4J6','cCkQ3QCarg');
 
-//$conn=mysqli_connect('localhost','ajay','0911ajay','feedback');
+$conn=mysqli_connect('localhost','ajay','0911ajay','feedback');
 if(!$conn){
 	echo "error:" . mysqli_connect_error();
 }
